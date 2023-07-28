d2d02034bedee5900d93d538598591474a8e11b8
Gaurav <guddugauravpp25@yahoo.co.uk>
2023-07-28 13:02:15 +0530

    update server js

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ connectToMongo();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
-// app.use(express.static(path.join(__dirname,"./frontend/build")));
-// app.get('*',(req,res)=>{
-//   res.sendFile(path.join(__dirname,"./frontend/build/index.html"))
-// })
+app.use(express.static(path.join(__dirname,"./frontend/build")));
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,"./frontend/build/index.html"))
+})
 
 
 const port = process.env.PORT || 3001
